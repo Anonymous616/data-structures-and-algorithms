f6101f7d2951eafeea2e1503a735ca69e89c5976
deon-gracias <deongracias1@gmail.com>
2022-05-18 14:04:32 +0530

    Fixed for loop constraints

--- Algorithms/Sorting/bubble-sort.c
+++ Algorithms/Sorting/bubble-sort.c
@@ -13,8 +13,8 @@ void swap(double *x,double *y) {
 */
 
 double* bubbleSort(double arr[], int n) {
-    for (int i = 0; i < n; i ++) {
-        for (int j = 0; j < i; j ++) {
+    for (int i = 0; i < n - 1; i ++) {
+        for (int j = 0; j < n - i - 1; j ++) {
             if (arr[j] > arr[j+1])
                 swap(&arr[j], &arr[j+1]);
 
